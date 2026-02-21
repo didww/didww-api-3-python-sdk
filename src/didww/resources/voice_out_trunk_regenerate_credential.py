87152cfd61702fd2fd5c6f3abed1e0df43b3495b
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-02-21 01:04:49 +0100

    Add set_voice_out_trunk() to VoiceOutTrunkRegenerateCredential
    Added voice_out_trunk relationship setter to match PHP/Java SDKs.

--- src/didww/resources/voice_out_trunk_regenerate_credential.py
+++ src/didww/resources/voice_out_trunk_regenerate_credential.py
@@ -5,6 +5,9 @@ class VoiceOutTrunkRegenerateCredential(BaseResource):
     _type = "voice_out_trunk_regenerate_credentials"
     _writable_attrs = set()
 
+    def set_voice_out_trunk(self, trunk_id):
+        self._set_relationship("voice_out_trunk", "voice_out_trunks", trunk_id)
+
 
 class VoiceOutTrunkRegenerateCredentialRepository(CreateOnlyRepository):
     _resource_class = VoiceOutTrunkRegenerateCredential
