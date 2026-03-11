98bdc49041539b672c8196dd5a625c17a5ddfd2c
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-11 18:48:42 +0100

    Add include=region test for NanpaPrefix with real API data (#30)

--- tests/resources/test_nanpa_prefix.py
+++ tests/resources/test_nanpa_prefix.py
@@ -22,16 +22,13 @@ class TestNanpaPrefix:
 
     @my_vcr.use_cassette("nanpa_prefixes/show_with_region.yaml")
     def test_find_nanpa_prefix_with_region(self, client):
-        params = QueryParams().include("country", "region")
-        response = client.nanpa_prefixes().find("6c16d51d-d376-4395-91c4-012321317e48", params)
+        params = QueryParams().include("region")
+        response = client.nanpa_prefixes().find("1e622e21-c740-4d3f-a615-2a7ef4991922", params)
         np = response.data
-        assert np.id == "6c16d51d-d376-4395-91c4-012321317e48"
-        assert np.npa == "864"
-        assert np.nxx == "920"
-        country = np.country
-        assert country is not None
-        assert country.name == "United States"
+        assert np.id == "1e622e21-c740-4d3f-a615-2a7ef4991922"
+        assert np.npa == "201"
+        assert np.nxx == "221"
         region = np.region
         assert region is not None
-        assert region.name == "South Carolina"
-        assert region.iso == "SC"
+        assert region.name == "New Jersey"
+        assert region.iso == "US-NJ"
