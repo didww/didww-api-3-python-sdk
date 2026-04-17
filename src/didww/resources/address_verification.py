0ae3d5cafc745b2bf6a6ac53ea88c1a4a97f885a
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 09:58:03 +0200

    feat!: address_verifications#reject_reasons is now an array of strings

--- src/didww/resources/address_verification.py
+++ src/didww/resources/address_verification.py
@@ -2,16 +2,6 @@ from didww.enums import AddressVerificationStatus, CallbackMethod
 from didww.resources.base import DidwwApiModel, DatetimeAttributeField, SafeAttributeField, EnumAttributeField, RelationField, CreateOnlyRepository
 
 
-class _SplitSemicolonField(SafeAttributeField):
-    """Splits a '; '-delimited (semicolon + space) string into a list."""
-
-    def __get__(self, instance, type=None):
-        value = super().__get__(instance, type)
-        if value is None:
-            return None
-        return value.split('; ')
-
-
 class AddressVerification(DidwwApiModel):
     _writable_attrs = {"service_description", "callback_url", "callback_method"}
 
@@ -19,7 +9,7 @@ class AddressVerification(DidwwApiModel):
     callback_url = SafeAttributeField("callback_url")
     callback_method = EnumAttributeField("callback_method", CallbackMethod)
     service_description = SafeAttributeField("service_description")
-    reject_reasons = _SplitSemicolonField("reject_reasons")
+    reject_reasons = SafeAttributeField("reject_reasons")
     reference = SafeAttributeField("reference")
     created_at = DatetimeAttributeField("created_at")
 
