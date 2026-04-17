e9154a74485cd44dac29da8639a5c0357ac978c6
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:13:50 +0200

    feat: add reject_comment and external_reference_id to AddressVerification for 2026-04-16

--- src/didww/resources/address_verification.py
+++ src/didww/resources/address_verification.py
@@ -3,14 +3,16 @@ from didww.resources.base import DidwwApiModel, DatetimeAttributeField, SafeAttr
 
 
 class AddressVerification(DidwwApiModel):
-    _writable_attrs = {"service_description", "callback_url", "callback_method"}
+    _writable_attrs = {"service_description", "callback_url", "callback_method", "external_reference_id"}
 
     status = EnumAttributeField("status", AddressVerificationStatus)
     callback_url = SafeAttributeField("callback_url")
     callback_method = EnumAttributeField("callback_method", CallbackMethod)
     service_description = SafeAttributeField("service_description")
     reject_reasons = SafeAttributeField("reject_reasons")
+    reject_comment = SafeAttributeField("reject_comment")
     reference = SafeAttributeField("reference")
+    external_reference_id = SafeAttributeField("external_reference_id")
     created_at = DatetimeAttributeField("created_at")
 
     address = RelationField("address")
